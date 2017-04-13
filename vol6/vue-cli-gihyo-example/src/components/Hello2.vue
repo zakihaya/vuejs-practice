72dc77e373315a241da1aec2faae008fa3c12729
hayazaki <toru.hayazaki@gmail.com>
2017-04-13 18:33:21 +0900

    Vol6 use vue-cli

---
+++ vol6/vue-cli-gihyo-example/src/components/Hello2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello2',
+    data() {
+      return {
+        message: 'ようこそ',
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
---
+++ vol6/vue-cli-gihyo-example/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import Hello2 from '@/components/Hello2';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/hello2',
+      name: 'Hello2',
+      component: Hello2,
+    },
+  ],
+});
